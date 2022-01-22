dcd5f0556ff08822d0e87b082d195342ba328489
sanahsidhu <sanaa.02.march@gmail.com>
2022-01-23 03:59:26 +0530

    added fake call code

--- server/mainapp/back.py
+++ server/mainapp/back.py
@@ -1,9 +1,18 @@
 import json
+import os
 from flask import *
 import requests
+from dotenv import load_dotenv
 from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
+
+load_dotenv()
+account_sid = os.getenv("twilio_sid")
+auth_token = os.getenv("twilio_token")
 
 app = Flask(__name__)
+
+
 @app.route("/sms", methods = ["POST", "GET"])
 def chatbot():
     bmsg = request.values.get('Body', '').lower()
@@ -28,3 +37,10 @@ def chatbot():
     msg.body(rep)
 
     return str(resp)
+
+
+def fake_call():
+    client = Client(account_sid,auth_token)
+    call = client.calls.create(to = os.getenv("my_phone_no"), from_ = "+16205829065", url = "http://demo.twilio.com/docs/voice.xml")             ##        get user mob no from session    !!!!
+    print(call.sid)
+
